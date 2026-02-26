fdc11eb974d64e866d0952cc20c299a47b8d1d5f
Ryan Carlos Duarte de Oliveira <g37365252@gmail.com>
2026-02-26 03:16:44 -0300

    Configurando porta para deploy

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def quimica():
 def biologia():
     return render_template("biologia.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
